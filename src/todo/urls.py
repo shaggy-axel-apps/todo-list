55ee4cd5fa4d4f74424751b000c1070ae0d7405f
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-03 11:48:02 +0300

    refactoring

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -10,7 +10,7 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
 ]
