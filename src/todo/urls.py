8fe8164c97025ec118ea3006e235648d7e543f37
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-03 09:08:05 +0300

    add `api/users/ with `UserViewSet`

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from apps.users.api.views import UserViewSet
+
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
