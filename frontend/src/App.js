0a21d1067bb8f1a2a1f38a37aa380e319bb48956
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-23 18:33:55 +0300

    add routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 import NavBar from './components/NavBar';
@@ -13,12 +15,24 @@ import IssueList from './components/Issue';
 
 const BASE_URL = "http://127.0.0.1:8000/"
 
+const pageNotFound404 = ({location}) => {
+  return (
+    <div>404 Error. Page '{location.pathname}' was not found</div>
+  )
+}
+
 class App extends React.Component {
 
   constructor(props) {
     super(props)
-    const user1 = {username: "shaggy", email: "shaggyaxel@mail.ru", firstName: "Ruslan", lastName: "Korneev"}
-    const user2 = {username: "dia_di", email: "dianakim@mail.ru", firstName: "Diana", lastName: "Pavlova"}
+    const user1 = {
+        username: "shaggy", email: "shaggyaxel@mail.ru",
+        firstName: "Ruslan", lastName: "Korneev"
+    }
+    const user2 = {
+        username: "dia_di", email: "dianakim@mail.ru",
+        firstName: "Diana", lastName: "Pavlova"
+    }
     const users = [user1, user2]
 
     const project1 = {
@@ -55,12 +69,20 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='app'>
-          <NavBar/>
-            <UserList users={this.state.users}/>
-            <ProjectList projects={this.state.projects}/>
-            <IssueList issues={this.state.issues}/>
-          <Footer/>
+      <div className={'App'}>
+        <NavBar/>
+          <BrowserRouter>
+            <Routes>
+              <Route path={'/'}
+                element={() => <UserList users={this.state.users}/>}/>
+              <Route path={'/projects/'}
+                element={() => <ProjectList projects={this.state.projects}/>}/>
+              <Route path={'/issues/'}
+                element={() => <IssueList issues={this.state.issues}/>}/>
+              <Route element={pageNotFound404}/>
+            </Routes>
+          </BrowserRouter>
+        <Footer/>
       </div>
     );
   };
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 export default class NavBar extends Component {
     render() {
         return <div>
-            <nav class="navbar navbar-light bg-dark">
-                <span class="navbar-brand mb-1 h1 text-white">Navbar</span>
+            <nav className="navbar navbar-light bg-dark">
+                <span className="navbar-brand mb-1 h1 text-white">Navbar</span>
             </nav>
         </div>;
     };
