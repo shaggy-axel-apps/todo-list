342bc185deea884a7a2cdd8962d582ed778def2a
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-23 19:39:41 +0300

    add requests to backend instead of constants

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,47 +25,24 @@ class App extends React.Component {
 
   constructor(props) {
     super(props)
-    const user1 = {
-        username: "shaggy", email: "shaggyaxel@mail.ru",
-        firstName: "Ruslan", lastName: "Korneev"
-    }
-    const user2 = {
-        username: "dia_di", email: "dianakim@mail.ru",
-        firstName: "Diana", lastName: "Pavlova"
-    }
-    const users = [user1, user2]
-
-    const project1 = {
-      contributors: users,
-      title: "todo-list",
-      repository: "https://github.com/shaggy-axel/todo-list/"
-    }
-    const projects = [project1]
-  
-    const issue1 = {
-        title: "some issue",
-        description: "some description",
-        project: project1,
-        isOpen: true,
-        assignees: [user1],
-        labels: []
-    }
-    const issues = [issue1]
-
     this.state = {
-      users: users,
-      projects: projects,
-      issues: issues
+      users: [],
+      projects: [],
+      issues: []
     };
   };
 
-  // componentDidMount() {
-  //   axios.get(`${BASE_URL}api/users/`).then(response => {
-  //     this.setState({"users": response.data.results})
-  //     console.log(response.data)
-  //     console.log(response.data.results)
-  //   }).catch(error => console.log(error))
-  // };
+  componentDidMount() {
+    axios.get(`${BASE_URL}api/users/`).then(response => {
+      this.setState({"users": response.data.results})
+    }).catch(error => console.log(error))
+    axios.get(`${BASE_URL}api/projects/`).then(response => {
+      this.setState({"projects": response.data.results})
+    }).catch(error => console.log(error))
+    axios.get(`${BASE_URL}api/issues/`).then(response => {
+      this.setState({"issues": response.data.results})
+    }).catch(error => console.log(error))
+  };
 
   render() {
     return (
