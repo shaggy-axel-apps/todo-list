05a4133daf1c21ab3cd58458df009dd174c549a4
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-26 08:55:24 +0300

    change user-list render using material-ui

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,49 +1,72 @@
-import React from "react";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 
-const UserItem = ({ user }) => {
-    return (
-      <tr className='table-primary'>
-        <td className='table-success'>
-          {user.username}
-        </td>
-        <td>
+const stringToColor = string => {
+  let hash = 0;
+  let i;
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let color = '#';
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.substr(-2);
+  }
+  return color;
+}
+
+
+const stringAvatar = name => {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+
+
+const UserItem = ({user}) => {
+  const fullName = `${user.firstName} ${user.lastName}`
+  return (
+    <ListItem alignItems="flex-start">
+      <ListItemAvatar>
+        <Avatar {...stringAvatar(fullName)} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={user.username}
+        secondary={
+        <React.Fragment>
+          <Typography
+          sx={{ display: 'inline' }}
+          component="span"
+          variant="body2"
+          color="text.primary"
+          >
           {user.email}
-        </td>
-        <td>
-          {user.firstName}
-        </td>
-        <td>
-          {user.lastName}
-        </td>
-      </tr>
-    )
+          </Typography>
+        </React.Fragment>
+        }
+      />
+    </ListItem>
+  )
 }
 
-const UserList = ({ users }) => {
-    return (
-      <div className="users">
-        <table className="table table-hover table-dark">
-          <thead>
-            <th scope="col">
-              <div>Username</div>
-            </th>
-            <th scope="col">
-              <div>Email</div>
-            </th>
-            <th scope="col">
-              <div>First name</div>
-            </th>
-            <th scope="col">
-              <div>Last name</div>
-            </th>
-          </thead>
-          <tbody>
-            {users.map((user) => <UserItem user={user} />)}
-          </tbody>
-        </table>
-      </div>
-    )
+
+const UserList = ({users}) => {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {/* {users.map((user) => <UserItem user={user}/>} */}
+      {users.map((user) => <UserItem user={user} />)}
+    </List>
+  );
 }
+
 
 export default UserList;
