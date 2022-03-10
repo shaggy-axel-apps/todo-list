9da2cc885a666b5bc9a6077a804bde6b63790c3e
Ruslan <kornerus@fjfi.cvut.cz>
2022-03-10 15:33:21 +0300

    test github tests

--- backend/src/apps/users/api/tests/test_viewset.py
+++ backend/src/apps/users/api/tests/test_viewset.py
@@ -35,6 +35,7 @@ class TestUserViewSet(TestCase):
     def test_post_users(self):
         response = self.client.post(reverse_lazy('users-list'), data=self.user_data_for_create)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_detail_user(self):
         response = self.client.get(reverse_lazy('users-detail', kwargs={'pk': self.user.id}))
